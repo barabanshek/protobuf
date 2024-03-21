6dcc669c9d61f23a5d33160be1d8139f13e989a6
Nikita <nikita@spr-ext-2.research.intel-research.net>
2024-03-21 12:10:41 -0700

    scatter-gather optimized

--- nikita_playground/fun/dsa/scatter_gather.h
+++ nikita_playground/fun/dsa/scatter_gather.h
@@ -45,25 +45,22 @@ class ScatterGather {
     memset(compl_batch_, 0, sizeof(completion_record));
 
     desc_pool_ = reinterpret_cast<hw_desc*>(
-        aligned_alloc(64, sizeof(hw_desc) * n_desc_));
+        aligned_alloc(PAGE_SIZE, sizeof(hw_desc) * n_desc_));
     if (desc_pool_ == nullptr) return -1;
-    comp_pool_ = reinterpret_cast<completion_record*>(
-        aligned_alloc(32, sizeof(completion_record) * n_desc_));
-    if (comp_pool_ == nullptr) return -1;
 
     memset(desc_pool_, 0, sizeof(hw_desc) * n_desc_);
-    memset(comp_pool_, 0, sizeof(completion_record) * n_desc_);
 
     // Pre-setup descriptors.
     for (size_t i = 0; i < n_desc_; ++i) {
-      desc_pool_[i].completion_addr =
-          reinterpret_cast<uint64_t>(&comp_pool_[i]);
-      desc_pool_[i].flags = dflags_;
+      desc_pool_[i].flags =
+          IDXD_OP_FLAG_CC;  // Don't generate individual completions, and do
+                            // cache control.
       desc_pool_[i].opcode = DSA_OPCODE_MEMMOVE;
     }
 
     desc_batch_->completion_addr = reinterpret_cast<uint64_t>(compl_batch_);
-    desc_batch_->flags = dflags_;
+    desc_batch_->flags =
+        IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_RCR;  // Request final completion.
     desc_batch_->opcode = DSA_OPCODE_BATCH;
     desc_batch_->desc_list_addr = reinterpret_cast<uint64_t>(desc_pool_);
 
@@ -81,7 +78,6 @@ class ScatterGather {
     size_t d_i = 0;
     size_t out_offset = 0;
     for (const auto& el : schema) {
-      comp_pool_[d_i].status = 0;
       desc_pool_[d_i].src_addr = reinterpret_cast<uint64_t>(std::get<0>(el));
       desc_pool_[d_i].dst_addr = reinterpret_cast<uint64_t>(out + out_offset);
       desc_pool_[d_i].xfer_size = std::get<1>(el);
@@ -94,6 +90,16 @@ class ScatterGather {
     return Submit(schema.size(), blocking);
   }
 
+  int GatherWithMemCpy(const Schema& schema, uint8_t* out, size_t* out_size) {
+    size_t out_offset = 0;
+    for (const auto& el : schema) {
+      memcpy(out + out_offset, std::get<0>(el), std::get<1>(el));
+      out_offset += std::get<1>(el);
+    }
+    *out_size = out_offset;
+    return 0;
+  }
+
   int Scatter(const uint8_t* in, const Schema& schema, bool blocking = true) {
     if (unlikely(schema.size() > n_desc_)) return -1;
 
@@ -103,7 +109,6 @@ class ScatterGather {
     size_t d_i = 0;
     size_t in_offset = 0;
     for (const auto& el : schema) {
-      comp_pool_[d_i].status = 0;
       desc_pool_[d_i].src_addr = reinterpret_cast<uint64_t>(in + in_offset);
       desc_pool_[d_i].dst_addr = reinterpret_cast<uint64_t>(std::get<0>(el));
       desc_pool_[d_i].xfer_size = std::get<1>(el);
@@ -115,9 +120,17 @@ class ScatterGather {
     return Submit(schema.size(), blocking);
   }
 
- private:
-  static constexpr int dflags_ = IDXD_OP_FLAG_CRAV | IDXD_OP_FLAG_RCR;
+  int ScatterWithMemCpy(const uint8_t* in, const Schema& schema) {
+    size_t in_offset = 0;
+    for (const auto& el : schema) {
+      memcpy(const_cast<uint8_t*>(std::get<0>(el)), in + in_offset,
+             std::get<1>(el));
+      in_offset += std::get<1>(el);
+    }
+    return 0;
+  }
 
+ private:
   inline int Submit(size_t n_desc, bool blocking = true) {
     compl_batch_->status = 0;
 
@@ -153,7 +166,6 @@ class ScatterGather {
   //
   size_t n_desc_;
   hw_desc* desc_pool_;
-  completion_record* comp_pool_;
 
   //
   hw_desc* desc_batch_;
--- nikita_playground/fun/dsa/scatter_gather_test.cc
+++ nikita_playground/fun/dsa/scatter_gather_test.cc
@@ -1,19 +1,29 @@
 #include "scatter_gather.h"
 
+#include <chrono>
 #include <iostream>
 #include <vector>
 
-static constexpr size_t N = 8;
-static constexpr size_t M = 1024;
+static constexpr size_t nOfIterations = 10000;
+
+// static constexpr size_t T = 1 * 1024 * 1024;
+static constexpr size_t N = 128;
+static constexpr size_t M = 128;
 
 int main() {
   // Data.
-  std::vector<std::vector<uint8_t>> scattered;
-  scattered.reserve(N);
-  for (size_t i = 0; i < N; ++i) {
-    scattered.push_back(std::vector<uint8_t>());
-    scattered.back().reserve(M);
-    for (size_t j = 0; j < M; ++j) scattered.back().push_back(i + j);
+  typedef std::vector<std::vector<uint8_t>> ScatteredObj;
+  typedef std::vector<uint8_t> GatheredStream;
+
+  std::vector<ScatteredObj> scattered_batch;
+  for (size_t it = 0; it < nOfIterations; ++it) {
+    ScatteredObj scattered_obj;
+    for (size_t i = 0; i < N; ++i) {
+      scattered_obj.push_back(std::vector<uint8_t>());
+      scattered_obj.back().reserve(M);
+      for (size_t j = 0; j < M; ++j) scattered_obj.back().push_back(it + i + j);
+    }
+    scattered_batch.push_back(scattered_obj);
   }
 
   // ScatterGather'er.
@@ -25,42 +35,62 @@ int main() {
   std::cout << "ScatterGather is initialized and ready" << std::endl;
 
   //
-  // Do Scatter.
-  //
-  std::vector<uint8_t> out(N * M, 0);
-  // <-------------- time start
-  ScatterGather::Schema schema;
-  schema.reserve(N);
-  for (const auto& el : scattered) {
-    schema.push_back(std::make_tuple(el.data(), el.size()));
-  }
-  size_t out_size = 0;
-  if (stuff.Gather(schema, out.data(), &out_size)) {
-    std::cout << "Failed to gather" << std::endl;
-    return -1;
-  }
-  assert(out.size() == out_size);
-  // -----------------> time stop
-
-  std::cout << "Gather done, out_size= " << out_size << " Bytes" << std::endl;
-
-  //
   // Do Gather.
   //
-  ScatterGather::Schema schema_gather;
-  std::vector<std::vector<uint8_t>> scattered_out;
-  scattered_out.reserve(N);
-  for (size_t i = 0; i < N; ++i) {
-    scattered_out.push_back(std::vector<uint8_t>(M, 1));
-    schema_gather.push_back(std::make_tuple(scattered_out.back().data(),
-                                            scattered_out.back().size()));
+  std::vector<GatheredStream> out(nOfIterations, GatheredStream(N * M, 0));
+  // <-------------- time start
+  auto begin = std::chrono::steady_clock::now();
+  for (size_t it = 0; it < scattered_batch.size(); ++it) {
+    ScatterGather::Schema schema;
+    schema.reserve(N);
+    for (const auto& el : scattered_batch[it]) {
+      schema.push_back(std::make_tuple(el.data(), el.size()));
+    }
+    size_t out_size = 0;
+    if (stuff.Gather(schema, out[it].data(), &out_size)) {
+      std::cout << "Failed to gather" << std::endl;
+      return -1;
+    }
+    assert(out[it].size() == out_size);
+  }
+  auto end = std::chrono::steady_clock::now();
+  auto took_ns =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+  std::cout << "gather took = " << took_ns / scattered_batch.size()
+            << std::endl;
+  // -----------------> time stop
+
+  std::cout << "Gather done" << std::endl;
+
+  //
+  // Do Scatter.
+  //
+  std::vector<ScatteredObj> scattered_out_batch;
+  for (size_t it = 0; it < nOfIterations; ++it) {
+    ScatteredObj scattered_obj;
+    for (size_t i = 0; i < N; ++i)
+      scattered_obj.push_back(std::vector<uint8_t>(M, 1));
+    scattered_out_batch.push_back(scattered_obj);
   }
 
   // <-------------- time start
-  if (stuff.Scatter(out.data(), schema_gather)) {
-    std::cout << "Failed to scatter" << std::endl;
-    return -1;
+  begin = std::chrono::steady_clock::now();
+  for (size_t it = 0; it < scattered_out_batch.size(); ++it) {
+    ScatterGather::Schema schema;
+    schema.reserve(N);
+    for (const auto& el : scattered_out_batch[it]) {
+      schema.push_back(std::make_tuple(el.data(), el.size()));
+    }
+    if (stuff.Scatter(out[it].data(), schema)) {
+      std::cout << "Failed to scatter" << std::endl;
+      return -1;
+    }
   }
+  end = std::chrono::steady_clock::now();
+  took_ns =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin).count();
+  std::cout << "scatter took = " << took_ns / scattered_out_batch.size()
+            << std::endl;
   // -----------------> time stop
 
   std::cout << "Scatter done" << std::endl;
@@ -69,9 +99,12 @@ int main() {
   // Verify.
   //
   bool correct = true;
-  for (size_t i = 0; i < N && correct; ++i) {
-    for (size_t j = 0; j < M && correct; ++j) {
-      if (scattered_out[i][j] != scattered[i][j]) correct = false;
+  for (size_t it = 0; it < nOfIterations && correct; ++it) {
+    for (size_t i = 0; i < N && correct; ++i) {
+      for (size_t j = 0; j < M && correct; ++j) {
+        if (scattered_out_batch[it][i][j] != scattered_batch[it][i][j])
+          correct = false;
+      }
     }
   }
   std::cout << (correct ? "ALL GOOD" : "DATA MISSMATCH") << std::endl;
