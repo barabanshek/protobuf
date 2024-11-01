9fc8086442b488cd04072cc5176e24c97b2bcd16
czarkos <chrisarkos16@gmail.com>
2024-11-01 12:50:46 -0700

    [testing] Hollistic proto compatibility correctness verification

--- tachidromos/proto_compatibility_verification/main.cc
+++ tachidromos/proto_compatibility_verification/main.cc
@@ -3,10 +3,11 @@
 #include <google/protobuf/util/message_differencer.h>
 
 #include"protos/test.pb.h"
+//#include"test.pb.h"
 #include"iaa_lib/scatter_gather.h"
 #include"iaa_lib/iaa_comp.h"
 
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 4*4096
 
 tachidromos::tests::SingularMessage InitializeSingularMessage() {
     tachidromos::tests::SingularMessage singular_message;
@@ -34,12 +35,87 @@ tachidromos::tests::SingularMessage InitializeSingularMessage() {
 
 tachidromos::tests::RepeatedMessage InitializeRepeatedMessage() {
     tachidromos::tests::RepeatedMessage repeated_message;
-    repeated_message.add_int_fields(1);
-    repeated_message.add_int_fields(2);
-    repeated_message.add_int_fields(3);
-    auto* repeated_nested_message = repeated_message.add_nested_messages();
-    repeated_nested_message->set_nested_string("World");
-    repeated_nested_message->set_nested_int(789);
+    // Initialize int_fields
+    for (int i = 0; i < 10; ++i) {
+        repeated_message.add_int_fields(i);
+    }
+
+    // Initialize long_fields
+    for (int64_t i = 0; i < 10; ++i) {
+        repeated_message.add_long_fields(i * 1000);
+    }
+    // Initialize uint_fields
+    for (uint32_t i = 0; i < 10; ++i) {
+        repeated_message.add_uint_fields(i * 10);
+    }
+
+    // Initialize ulong_fields
+    for (uint64_t i = 0; i < 10; ++i) {
+        repeated_message.add_ulong_fields(i * 10000);
+    }
+
+    // Initialize sint_fields
+    for (int32_t i = 0; i < 10; ++i) {
+        repeated_message.add_sint_fields(i * -1);
+    }
+
+    // Initialize slong_fields
+    for (int64_t i = 0; i < 10; ++i) {
+        repeated_message.add_slong_fields(i * -1000);
+    }
+
+    // Initialize fixed_fields
+    for (uint32_t i = 0; i < 10; ++i) {
+        repeated_message.add_fixed_fields(i * 100);
+    }
+
+    // Initialize lfixed_fields
+    for (uint64_t i = 0; i < 10; ++i) {
+        repeated_message.add_lfixed_fields(i * 100000);
+    }
+
+    // Initialize sfixed_fields
+    for (int32_t i = 0; i < 10; ++i) {
+        repeated_message.add_sfixed_fields(i * -100);
+    }
+
+    // Initialize slfixed_fields
+    for (int64_t i = 0; i < 10; ++i) {
+        repeated_message.add_slfixed_fields(i * -100000);
+    }
+
+    // Initialize float_fields
+    for (int i = 0; i < 10; ++i) {
+        repeated_message.add_float_fields(i * 1.1f);
+    }
+
+    // Initialize double_fields
+    for (int i = 0; i < 10; ++i) {
+        repeated_message.add_double_fields(i * 1.1);
+    }
+
+    // Initialize bool_fields
+    for (int i = 0; i < 10; ++i) {
+        repeated_message.add_bool_fields(i % 2 == 0);
+    }
+
+    // Initialize string_fields
+    for (int i = 0; i < 10; ++i) {
+        repeated_message.add_string_fields("string_" + std::to_string(i));
+    }
+
+    // Initialize bytes_fields
+    for (int i = 0; i < 10; ++i) {
+        repeated_message.add_bytes_fields("bytes_" + std::to_string(i));
+    }
+
+    // Initialize nested_messages
+    for (int i = 0; i < 10; ++i) {
+        auto* nested_message = repeated_message.add_nested_messages();
+        nested_message->set_nested_string("nested_string_" + std::to_string(i));
+        nested_message->set_nested_int(i);
+    }
+
     return repeated_message;
 }
 
@@ -96,7 +172,100 @@ int main(int argc, char* argv[]) {
 	//iaa_init_jobs(qpl_path_hardware);
 	iaa_init_jobs(qpl_path_software);
 
-    if (argc > 1 && std::string(argv[1]) == "ri") {
+    if (argc > 1 && std::string(argv[1]) == "rr") {
+        // Initialize RepeatedMessage
+        tachidromos::tests::RepeatedMessage repeated_message = InitializeRepeatedMessage();
+
+        // Generate gather schema
+        ScatterGather::Schema gather_schema;
+        repeated_message.generate_schema(gather_schema);
+        //gather_schema.push_back(std::make_tuple(reinterpret_cast<uint8_t*>(const_cast<int32_t*>(int_repeated_message.mutable_int_fields()->mutable_data())), int_repeated_message.int_fields_size() * sizeof(int32_t)));
+        // Generate sizes for scatter
+        std::vector<size_t> scatter_sizes;
+        //scatter_sizes.push_back(int_repeated_message.int_fields_size() * sizeof(int32_t));
+        repeated_message.generate_scatter_sizes(scatter_sizes);
+
+        // GATHER + COMPRESSION
+        ScatterGather scagatherer;
+        size_t out_size = 0;
+        std::vector<uint8_t> gather_out(BUFFER_SIZE, 0);
+        // gather
+        auto outcome = scagatherer.GatherWithMemCpy(gather_schema, gather_out.data(), &out_size);
+        if (outcome) {
+            std::cerr << "Failed to gather" << std::endl;
+            return -1;
+        }
+        // compress
+        uint8_t* compressed   = new uint8_t[BUFFER_SIZE];
+        uint32_t comprOutputSize = 0;
+        outcome = compress_with_IAA(gather_out.data(), out_size, compressed, BUFFER_SIZE, &comprOutputSize);
+        if (outcome) {
+            std::cerr << "Failed to compress" << std::endl;
+            return -1;
+        }
+
+        // DECOMPRESSION + SCATTER
+        tachidromos::tests::RepeatedMessage out_message;
+        out_message.mutable_int_fields()->Resize(repeated_message.int_fields_size(), 0);
+        out_message.mutable_long_fields()->Resize(repeated_message.long_fields_size(), 0);
+        out_message.mutable_uint_fields()->Resize(repeated_message.uint_fields_size(), 0);
+        out_message.mutable_ulong_fields()->Resize(repeated_message.ulong_fields_size(), 0);
+        out_message.mutable_sint_fields()->Resize(repeated_message.sint_fields_size(), 0);
+        out_message.mutable_slong_fields()->Resize(repeated_message.slong_fields_size(), 0);
+        out_message.mutable_fixed_fields()->Resize(repeated_message.fixed_fields_size(), 0);
+        out_message.mutable_lfixed_fields()->Resize(repeated_message.lfixed_fields_size(), 0);
+        out_message.mutable_sfixed_fields()->Resize(repeated_message.sfixed_fields_size(), 0);
+        out_message.mutable_slfixed_fields()->Resize(repeated_message.slfixed_fields_size(), 0);
+        out_message.mutable_float_fields()->Resize(repeated_message.float_fields_size(), 0);
+        out_message.mutable_double_fields()->Resize(repeated_message.double_fields_size(), 0);
+        out_message.mutable_bool_fields()->Resize(repeated_message.bool_fields_size(), 0);
+        for (int i = 0; i < repeated_message.string_fields_size(); i++) {
+            out_message.add_string_fields("$$$$$$$#");
+        }
+        for (int i = 0; i < repeated_message.bytes_fields_size(); i++) {
+            out_message.add_bytes_fields("$$$$$$#");
+        }
+        for (int i = 0; i < repeated_message.nested_messages_size(); i++) {
+            auto* nested_message = out_message.add_nested_messages();
+            nested_message->set_nested_string("$$$$$$$$$$$$$$#");
+        }
+        //out_message.mutable_nested_messages()->Resize(repeated_message.nested_messages_size(), 0);
+        ScatterGather::Schema scatter_schema;
+        out_message.generate_schema(scatter_schema);
+        
+        uint8_t* decompressed   = new uint8_t[BUFFER_SIZE];
+        uint32_t decomprOutputSize = 0;
+        outcome = decompress_with_IAA(compressed, comprOutputSize, decompressed, BUFFER_SIZE, &decomprOutputSize);
+        if (outcome) {
+            std::cerr << "Failed to decompress" << std::endl;
+            return -1;
+        }
+        outcome = scagatherer.ScatterWithMemCpy(decompressed, scatter_schema);
+        if (outcome) {
+            std::cerr << "Failed to scatter" << std::endl;
+            return -1;
+        }
+
+        //std::cout << int_repeated_message.DebugString() << std::endl;
+        //std::cout << out_message.DebugString() << std::endl;
+
+        bool correct = google::protobuf::util::MessageDifferencer::Equivalent(repeated_message, out_message);
+        std::cout << "Messages are equivalent: " << (correct ? "true" : "false") << std::endl;
+
+        /*
+        // Serialize RepeatedMessage
+        std::string repeated_serialized;
+        repeated_message.SerializeToString(&repeated_serialized);
+
+        // Deserialize RepeatedMessage
+        tachidromos::tests::RepeatedMessage repeated_message_deserialized;
+        repeated_message_deserialized.ParseFromString(repeated_serialized);
+
+        // Print RepeatedMessage
+        std::cout << "Deserialized RepeatedMessage: " << repeated_message_deserialized.DebugString() << std::endl;
+        */
+    }
+    else if (argc > 1 && std::string(argv[1]) == "ri") {
         // Initialize RepeatedMessage
         tachidromos::tests::IntRepeatedMessage int_repeated_message = InitializeIntRepeatedMessage();
 
@@ -173,10 +342,8 @@ int main(int argc, char* argv[]) {
         // Generate gather schema
         ScatterGather::Schema gather_schema;
         string_repeated_message.generate_schema(gather_schema);
-        //gather_schema.push_back(std::make_tuple(reinterpret_cast<uint8_t*>(const_cast<int32_t*>(int_repeated_message.mutable_int_fields()->mutable_data())), int_repeated_message.int_fields_size() * sizeof(int32_t)));
         // Generate sizes for scatter
         std::vector<size_t> scatter_sizes;
-        //scatter_sizes.push_back(int_repeated_message.int_fields_size() * sizeof(int32_t));
         string_repeated_message.generate_scatter_sizes(scatter_sizes);
 
         // GATHER + COMPRESSION
@@ -190,7 +357,14 @@ int main(int argc, char* argv[]) {
             return -1;
         }
         // compress
-        uint8_t* compressed   = new uint8_t[BUFFER_SIZE];
+        uint8_t* compressed   = nullptr;
+        try {
+            compressed   = new uint8_t[BUFFER_SIZE];
+        }
+        catch (std::bad_alloc& ba) {
+            std::cerr << "Failed to allocate memory for compressed buffer: " << ba.what() << std::endl;
+            return -1;
+        }
         uint32_t comprOutputSize = 0;
         outcome = compress_with_IAA(gather_out.data(), out_size, compressed, BUFFER_SIZE, &comprOutputSize);
         if (outcome) {
@@ -206,7 +380,14 @@ int main(int argc, char* argv[]) {
         ScatterGather::Schema scatter_schema;
         out_message.generate_schema(scatter_schema);
         
-        uint8_t* decompressed   = new uint8_t[BUFFER_SIZE];
+        uint8_t* decompressed = nullptr;
+        try {
+            decompressed   = new uint8_t[BUFFER_SIZE];
+        }
+        catch (std::bad_alloc& ba) {
+            std::cerr << "Failed to allocate memory for decompressed buffer: " << ba.what() << std::endl;
+            return -1;
+        }
         uint32_t decomprOutputSize = 0;
         outcome = decompress_with_IAA(compressed, comprOutputSize, decompressed, BUFFER_SIZE, &decomprOutputSize);
         if (outcome) {
@@ -217,7 +398,8 @@ int main(int argc, char* argv[]) {
         if (outcome) {
             std::cerr << "Failed to scatter" << std::endl;
             return -1;
-        }
+        } 
+
 
         //std::cout << int_repeated_message.DebugString() << std::endl;
         //std::cout << out_message.DebugString() << std::endl;
